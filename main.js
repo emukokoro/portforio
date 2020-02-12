1617c39369a2d633e9f50c633585e498b78a9cdf
emukokoro <emukokoro@gmail.com>
2020-02-12 08:19:01 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//マウスオーバー時の処理
+function mouseOn(){
+	let about = document.getElementById("about");
+	about.src = "images/letter-about.png";
+}
+
+//マウスアウト時の処理
+
+function mouseOff(){
+	let about = document.getElementById("about");
+	about.src = "images/letter-about-cl.png";
+}
+
+
+function mouseOn2(){
+	let works = document.getElementById("works");
+	works.src = "images/letter-works.png";
+}
+
+function mouseOff2(){
+	let works = document.getElementById("works");
+	works.src = "images/letter-works-cl.png";
+}
+
+//マウスアウト時の処理
+
+function mouseOff3(){
+	let blog = document.getElementById("blog");
+	blog.src = "images/letter-blog-cl.png";
+}
+
+function mouseOn3(){
+	let blog = document.getElementById("blog");
+	blog.src = "images/letter-blog.png";
+}
